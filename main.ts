413e8abf06e2ced7ddb3a54548ea3e65042bb4b1
TahaNamdar <tahanamdar30@gmail.com>
2024-06-19 21:13:44 +0330

    closure

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
- 
+//closure =========================================
+
+const createAdder = (a: number) => {
+  function add(b: number) {
+    return a + b;
+  }
+  return add;
+};
+
+const f = createAdder(5);
+console.log(f(10));
+
+const increment = () => {
+  let counter = 0;
+
+  function incrementCounter() {
+    return ++counter;
+  }
+
+  return {
+    result: incrementCounter,
+  };
+};
+
+const counter1 = increment();
+const counter2 = increment();
+
+console.log(counter1.result());
+console.log(counter2.result());
+
+//closure =========================================
